533db709ca643011898fea9114838c3cc550b012
Wiktor Wrona <wronawiktor@gmail.com>
2022-03-24 15:30:57 +0100

    Second version
    Changelog:
    1. Deleted "sudo" from all commands
    2. Changed names of variables to more accurate
    3. Inverted logic in "terraform install" checking command
    4. Deleted "terraform plan" and "terraform apply"

--- install.sh
+++ install.sh
@@ -5,32 +5,32 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 # Chceck is there any new packages
-sudo apt update && apt upgrade --yes
+apt update && apt upgrade --yes
 echo "Upgrade complete"
 
 # Install required packages
-sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager git-core libguestfs-tools jq
+apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager git-core libguestfs-tools jq
 echo "Install complete"
 
 # Add your user to the libvirt and kvm group
-sudo usermod -a -G kvm $(whoami)
-sudo usermod -a -G libvirt $(whoami)
+usermod -a -G kvm $(whoami)
+usermod -a -G libvirt $(whoami)
 echo "Added your user to the libvirt and kvm group"
 
 # Check user permissions
 
 # Set security_driver = "none"
-COMMAND="$(sudo grep "security_driver = \"none\"" /etc/libvirt/qemu.conf)"
-COMMAND=${COMMAND// /}
-if [[ "$COMMAND" != 'security_driver="none"' ]] ; then
-    sudo sed -i -e 's/#security_driver = "selinux"/security_driver = "none"/g' /etc/libvirt/qemu.conf
+LIBVIRT_SEC_DRIVER="$(grep "security_driver = \"none\"" /etc/libvirt/qemu.conf)"
+LIBVIRT_SEC_DRIVER=${LIBVIRT_SEC_DRIVER// /}
+if [[ "$LIBVIRT_SEC_DRIVER" != 'security_driver="none"' ]] ; then
+    sed -i -e 's/#security_driver = "selinux"/security_driver = "none"/g' /etc/libvirt/qemu.conf
     echo "security_driver set to none"
 else
     echo "security_driver is already set to none"
 fi
 
 # Start libvirtd
-sudo systemctl start libvirtd
+systemctl start libvirtd
 # sudo systemctl status libvirtd
 if [ pidof libvirtd != "" ]; then
     echo "Libvirt is running"
@@ -41,7 +41,7 @@ fi
 
 # Download Linux cloud image
 IMG_PATH ="/var/lib/libvirt/images/bionic-server-cloudimg-amd64.img"
-sudo  curl -L https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img --output $IMG_PATH
+curl -L https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img --output $IMG_PATH
 if [ -f "$IMG_PATH" ]; then
     echo "System image was downloaded"
 else
@@ -50,15 +50,15 @@ else
 fi
 
 # Install Terraform
-sudo -i
-sudo curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt install terraform
-if ! command -v terraform &> /dev/null; then
-    echo "Terraform installation error"
-    exit 1
+-i
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt install terraform
+if command -v terraform &> /dev/null; then
+    echo "Terraform installation complete"
 else
-    echo "Terraform installation complete" 
+    echo "Terraform installation error"
+    exit 1 
 fi
 
 # Clone cloud-infra
@@ -80,14 +80,14 @@ fi
 
 # Copy your ssh-key
 if [ -f "~/.ssh/id_rsa.pub" ]; then
-    KEY = cat ~/.ssh/id_rsa.pub
+    SSH_PUB_KEY = cat ~/.ssh/id_rsa.pub
     TEMPLATE=authorized_keys = [
         ""
     ]
     TEMPLATE_K = authorized_keys = [
-        "${KEY}"
+        "${SSH_PUB_KEY}"
     ]
-    sudo sed -i -e 's/${TEMPLATE}/${TEMPLATE_K}/g' $TF_SET_FILE
+    sed -i -e 's/${TEMPLATE}/${TEMPLATE_K}/g' $TF_SET_FILE
     echo "Added your ssh key to terraform settings"
 else
     echo "No public ssh key to copy, you can generate it by 'ssh-keygen' "
@@ -95,7 +95,7 @@ fi
 
 # Start Terraform
 terraform init
-terraform plan
-terraform apply --auto-approve
+# terraform plan
+# terraform apply --auto-approve
 
 exit 0
