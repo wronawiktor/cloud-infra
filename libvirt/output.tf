fea5cb634fda1bbfc6fec3e7907ab35b36ac8abd
Wiktor Wrona <wronawiktor@gmail.com>
2022-04-07 12:37:26 +0200

    Added creating host file feature Fixes: #7

--- libvirt/output.tf
+++ libvirt/output.tf
@@ -11,3 +11,23 @@ output "ip_workers" {
     libvirt_domain.worker.*.network_interface.0.addresses,
   )
 }
+
+resource "local_file" "hosts" {
+  filename = "/tmp/terraform-hosts"
+  content  = <<-EOT
+# Managed by Terraform
+%{ for id in libvirt_domain.control_plane ~}
+${id.network_interface.0.addresses.0} ${id.network_interface.0.hostname}
+%{ endfor ~}
+%{ for id in libvirt_domain.worker ~}
+${id.network_interface.0.addresses.0} ${id.network_interface.0.hostname}
+%{ endfor ~}
+EOT
+}
+
+resource "null_resource" "hosts" {
+  depends_on = [local_file.hosts]
+  provisioner "local-exec" {
+    command = "bash generate-hosts.sh"
+  }
+}
