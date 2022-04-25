0c02afbc6d01371dc0b092a40cc77c0e93e44757
Michal Jura <mjura@suse.com>
2022-04-25 11:46:30 +0200

    Fix helm version v3.8.2

--- libvirt/provision-k8s-helm.sh
+++ libvirt/provision-k8s-helm.sh
@@ -5,5 +5,6 @@ if [ "$(id -u)" != "0" ]; then
   exec sudo "$0" "$@"
 fi
 
-HELM_VERSION=$(curl -s https://api.github.com/repos/helm/helm/releases/latest | jq -r '.tag_name')
+#HELM_VERSION=$(curl -s https://api.github.com/repos/helm/helm/releases/latest | jq -r '.tag_name')
+HELM_VERSION="v3.8.2"
 curl -sSL "https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz" | sudo tar -C /usr/local/bin --strip-components=1 -xzf - linux-amd64/helm
